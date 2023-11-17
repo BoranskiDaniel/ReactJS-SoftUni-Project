606115abedf1a24dc822944de2b1ad700ca06ab5
Daniel Boranski <dboranski@gmail.com>
2023-11-17 22:48:20 +0200

    added order modal (pt1)

--- client/src/components/order/Order.jsx
+++ client/src/components/order/Order.jsx
@@ -2,11 +2,10 @@ import styles from "./Order.module.css"
 
 export default function Order() {
     return (
-        <form>
+        <form className={styles.mainOrder}>
             <div>
                 <label className={styles.label} htmlFor="username">Username</label>
-                <input className={errors.username && styles.inputError}
-                    ref={usernameInputRef}
+                <input
                     type="text"
                     id="username"
                     name="username"
@@ -16,7 +15,7 @@ export default function Order() {
             </div>
             <div>
                 <label className={styles.label} htmlFor="company">Company</label>
-                <input className={errors.username && styles.inputError}
+                <input
                     type="text"
                     id="company"
                     name="company"
@@ -25,8 +24,8 @@ export default function Order() {
                 />
             </div>
             <div>
-                <button type="submit" onClick={submitHandler}>Register</button>
-                <button type="button" onClick={resetFormHandler}>Reset</button>
+                <button type="submit">Register</button>
+                <button type="button">Reset</button>
             </div>
         </form>
     );
--- client/src/components/productCard/ProductCard.jsx
+++ client/src/components/productCard/ProductCard.jsx
@@ -1,4 +1,4 @@
-// import Order from "../order/Order";
+import Order from "../order/Order";
 import styles from "./ProductCard.module.css";
 // import { Link } from "react-router-dom";
 import { useState } from "react";
@@ -16,37 +16,35 @@ export default function ProductCard({
 
     const orderHandler = () => {
         setOrder(true);
-        console.log('hihi');
     }
     return (
-        <div className={styles.articles}>
-            <article>
-                <div className={styles.articleWrapper}>
-                    <figure className={styles.cardFigure}>
-                        <img src={imageUrl} alt={`${name}`} />
-                    </figure>
-                    <div className={styles.articleBody}>
-                        <h2>{name}</h2>
-                        <p>Type: {type}</p>
-                        <p>Sort: {sort}</p>
-                        <p>Company: {company}</p>
-                        <p className={styles.price}>Price: {price || negotiable}</p>
-                        <a className={styles.readMore}>
-                            {order && (<>
-                                <p>Text here</p>
-                            </>)}
-                            <button className={styles.order} onClick={orderHandler}> Order</button>
-                            <svg xmlns="http://www.w3.org/2000/svg" className={styles.icon} viewBox="0 0 20 20" fill="currentColor">
-                                <path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
-                            </svg>
-                        </a>
-                        <div className={styles.forOwner}>
-                            <button > Edit</button>
-                            <button > Detelete</button>
+        <>
+            <div className={styles.articles}>
+                <article onClick={orderHandler}>
+                    <div className={styles.articleWrapper}>
+                        <figure className={styles.cardFigure}>
+                            <img src={imageUrl} alt={`${name}`} />
+                        </figure>
+                        <div className={styles.articleBody}>
+                            <h2>{name}</h2>
+                            <p>Type: {type}</p>
+                            <p>Sort: {sort}</p>
+                            <p>Company: {company}</p>
+                            <p className={styles.price}>Price: {price || negotiable}</p>
+                            <a className={styles.readMore}>
+                                <svg xmlns="http://www.w3.org/2000/svg" className={styles.icon} viewBox="0 0 20 20" fill="currentColor">
+                                    <path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
+                                </svg>
+                            </a>
+                            <div className={styles.forOwner}>
+                                <button > Edit</button>
+                                <button > Detelete</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </article>
-        </div>
+                </article>
+            </div>
+            {order && <Order />}
+        </>
     );
 };
