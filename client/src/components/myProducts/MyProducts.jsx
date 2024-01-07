a70b0ed26aa6aaabc1c59565cfc5fc15d7af596b
Daniel Boranski <dboranski@gmail.com>
2024-01-07 18:41:55 +0200

    fixed myProduct page

--- client/src/components/myProducts/MyProducts.jsx
+++ client/src/components/myProducts/MyProducts.jsx
@@ -17,8 +17,22 @@ export default function MyProducts({
 
     // console.log(...showMyProduct);
     let element = '';
+    let imageUrl = '';
+    let name = '';
+    let type = '';
+    let sort = '';
+    let company = '';
+    let price = '';
+
     for (let index = 0; index < showMyProduct.length; index++) {
         element = showMyProduct[index]._ownerId;
+        imageUrl = showMyProduct[index].imageUrl;
+        name = showMyProduct[index].name;
+        type = showMyProduct[index].type;
+        sort = showMyProduct[index].sort;
+        company = showMyProduct[index].company;
+        price = showMyProduct[index].price;
+
         console.log(element);
     }
     console.log(userId);
@@ -33,14 +47,14 @@ export default function MyProducts({
                     <article >
                         <div className={styles.articleWrapper}>
                             <figure className={styles.cardFigure}>
-                                <img src={showMyProduct.imageUrl} alt={`${showMyProduct.name}`} />
+                                <img src={imageUrl} alt={name} />
                             </figure>
                             <div className={styles.articleBody}>
-                                <h2>{showMyProduct.name}</h2>
-                                <p>Type: {showMyProduct.type}</p>
-                                <p>Sort: {showMyProduct.sort}</p>
-                                <p>Company: {showMyProduct.company}</p>
-                                <p className={styles.price}>Price ($ per kg. ): {showMyProduct.price || showMyProduct.negotiable}</p>
+                                <h2>{name}</h2>
+                                <p>Type: {type}</p>
+                                <p>Sort: {sort}</p>
+                                <p>Company: {company}</p>
+                                <p className={styles.price}>Price ($ per kg. ): {price}</p>
                             </div>
                         </div>
                     </article>
